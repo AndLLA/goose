75ebc8f0374e5517f8cfa6cc8b1cc01779ac4a03
user <44858649+AndLLA@users.noreply.github.com>
2020-02-15 01:11:25 +0100

    more stats

--- goose/src/goose/MainWoprOnce.java
+++ goose/src/goose/MainWoprOnce.java
@@ -8,26 +8,29 @@ public class MainWoprOnce {
 		// 1 player - 4 rolls
 		new ConsoleGameWopr(1953).run();
 		
+		// 1 player - 210 rolls
+		new ConsoleGameWopr(13499664).run();
+		
 		// 1 player - 159 rolls
 		new ConsoleGameWopr(388691).run();
 		
 		// 9 players - 4 rolls 
 		new ConsoleGameWopr(1263).run();
 		
-		// 9 players - 24 rolls 
-		new ConsoleGameWopr(1483679).run();
+		// 9 players - 27 rolls 
+		new ConsoleGameWopr(14982468).run();
 		
 		/*
-			  numPlayers minDicesRolled maxDicesRolled minSeed maxSeed
-			1          1              4            159    1953  388691
-			2          2              4             87     634  641197
-			3          3              4             55     362  941110
-			4          4              4             43    2437  181514
-			5          5              4             34     458  530396
-			6          6              4             31     157  179609
-			7          7              4             28    1351  928829
-			8          8              4             24     652  606235
-			9          9              4             24    1263 1483679
+			  numPlayers minDicesRolled maxDicesRolled maxDicesRolledTot minSeed  maxSeed
+			1          1              4            210               210    1953 13499664
+			2          2              4             98               196     634 11144320
+			3          3              4             62               186     362  5942340
+			4          4              4             46               184    2437  9304955
+			5          5              4             39               195     458  8619631
+			6          6              4             35               210     157  6252911
+			7          7              4             29               203    1351 10461846
+			8          8              4             27               216     652 16463519
+			9          9              4             27               243    1263 14982468
 		 */
 		
 	}
